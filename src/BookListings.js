7143d66ad02d06ce688bd18847eb2ac5c7744e47
Aqueum <martin@aqueum.com>
2017-08-29 00:02:19 +0100

    Filter books

--- src/BookListings.js
+++ src/BookListings.js
@@ -4,41 +4,54 @@ import PropTypes from 'prop-types';
 // eslint-disable-next-line
 import * as BooksAPI from './BooksAPI';
 import './App.css';
+import escapeRegExp from 'escape-string-regexp';
 
 class BookListings extends Component {
   static propTypes = {};
 
   render() {
+    const { books } = this.props;
+    const { query } = this.state;
+    let showingBooks;
+    if (query) {
+      const match = new RegExp(escapeRegExp(query), 'i');
+      showingBooks = books.filter(book => match.test(book.name));
+    } else {
+      showingBooks = books;
+    }
+
     return (
       <ol className="books-grid">
-        <li>
-          <div className="book">
-            <div className="book-top">
-              <div
-                className="book-cover"
-                style={{
-                  width: 128,
-                  height: 193,
-                  backgroundImage:
-                    'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")'
-                }}
-              />
-              <div className="book-shelf-changer">
-                <select>
-                  <option value="none" disabled>
-                    Move to...
-                  </option>
-                  <option value="currentlyReading">Currently Reading</option>
-                  <option value="wantToRead">Want to Read</option>
-                  <option value="read">Read</option>
-                  <option value="none">None</option>
-                </select>
+        {showingBooks.map(book =>
+          <li>
+            <div className="book">
+              <div className="book-top">
+                <div
+                  className="book-cover"
+                  style={{
+                    width: 128,
+                    height: 193,
+                    backgroundImage:
+                      'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")'
+                  }}
+                />
+                <div className="book-shelf-changer">
+                  <select>
+                    <option value="none" disabled>
+                      Move to...
+                    </option>
+                    <option value="currentlyReading">Currently Reading</option>
+                    <option value="wantToRead">Want to Read</option>
+                    <option value="read">Read</option>
+                    <option value="none">None</option>
+                  </select>
+                </div>
               </div>
+              <div className="book-title">To Kill a Mockingbird</div>
+              <div className="book-authors">Harper Lee</div>
             </div>
-            <div className="book-title">To Kill a Mockingbird</div>
-            <div className="book-authors">Harper Lee</div>
-          </div>
-        </li>
+          </li>
+        )}
       </ol>
     );
   }
