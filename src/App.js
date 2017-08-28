332c230f5c78655a48816952985eec66e6513e9e
Aqueum <martin@aqueum.com>
2017-08-28 22:21:27 +0100

    Import & use Component from react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 // eslint-disable-next-line
 import * as BooksAPI from './BooksAPI';
 import BookListing from './BookListing';
 import './App.css';
 
-class BooksApp extends React.Component {
+class BooksApp extends Component {
   state = {
     /**
      * TODO: Instead of using this state variable to keep track of which page
