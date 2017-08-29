c3341c4fe3eadf9476b7c26398c0be97a3c820ab
Aqueum <martin@aqueum.com>
2017-08-29 09:06:50 +0100

    Get book data from API

--- src/BookListings.js
+++ src/BookListings.js
@@ -25,7 +25,7 @@ class BookListings extends Component {
     return (
       <ol className="books-grid">
         {showingBooks.map(book =>
-          <li>
+          <li key={book.id}>
             <div className="book">
               <div className="book-top">
                 <div
@@ -33,8 +33,7 @@ class BookListings extends Component {
                   style={{
                     width: 128,
                     height: 193,
-                    backgroundImage:
-                      'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")'
+                    backgroundImage: `url(${book.imageLinks.smallThumbnail})`
                   }}
                 />
                 <div className="book-shelf-changer">
@@ -49,8 +48,12 @@ class BookListings extends Component {
                   </select>
                 </div>
               </div>
-              <div className="book-title">To Kill a Mockingbird</div>
-              <div className="book-authors">Harper Lee</div>
+              <div className="book-title">
+                {book.title}
+              </div>
+              <div className="book-authors">
+                {book.authors}
+              </div>
             </div>
           </li>
         )}
