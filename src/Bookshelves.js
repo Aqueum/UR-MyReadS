38acd9185f8aa5d7252ff603755b9d47f188046a
Aqueum <martin@aqueum.com>
2017-08-29 16:25:32 +0100

    Remove extraneous div

--- src/Bookshelves.js
+++ src/Bookshelves.js
@@ -35,21 +35,16 @@ class Bookshelves extends Component {
           <h1>MyReads</h1>
         </div>
         <div className="list-books-content">
-          <div>
-            {shelfNames.map(shelfName =>
-              <div className="bookshelf" key={shelfName.mac}>
-                <h2 className="bookshelf-title">
-                  {shelfName.human}
-                </h2>
-                <div className="bookshelf-books">
-                  <BookListings
-                    books={this.state.books}
-                    shelf={shelfName.mac}
-                  />
-                </div>
+          {shelfNames.map(shelfName =>
+            <div className="bookshelf" key={shelfName.mac}>
+              <h2 className="bookshelf-title">
+                {shelfName.human}
+              </h2>
+              <div className="bookshelf-books">
+                <BookListings books={this.state.books} shelf={shelfName.mac} />
               </div>
-            )}
-          </div>
+            </div>
+          )}
         </div>
         <div className="open-search">
           <Link to="/search">Add a book</Link>
